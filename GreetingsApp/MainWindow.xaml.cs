516984b8e270c20149fe7b5f99e5e7aeff487975
jeffors <register.uzik@yandex.ru>
2022-03-22 13:14:56 +0500

    Finalize

--- GreetingsApp/MainWindow.xaml.cs
+++ GreetingsApp/MainWindow.xaml.cs
@@ -1,21 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
 using System.Text.Json;
-using System.Text.Unicode;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GreetingsApp
 {
@@ -24,43 +11,49 @@ namespace GreetingsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-
-            var langsMessage = new LangsReader
-            {
-                LangMessages = new Dictionary<string, TimesOfDay>
-                {
-                    ["English"] = new TimesOfDay { Day = "Good Day", Evening = "Good Evening", Morning = "Good Morning", Night = "Good Night" },
-                    ["Deutsch"] = new TimesOfDay { Day = "Guten Tag", Evening = "Guten Abend", Morning = "Guten Morgen", Night = "Gute Nacht" },
-                    ["Russian"] = new TimesOfDay { Day = "Добрый день", Evening = "Добрый вечер", Morning = "Доброе утро", Night = "Доброй ночи" },
-                    ["Татарча"] = new TimesOfDay { Day = "Хәерле көн", Evening = "Хәерле кич", Morning = "Хәерле иртә", Night = "Хәерле төн" }
-                }
-            };
-
-            string fileName = "langs.json";
-            var options = new JsonSerializerOptions { WriteIndented = true, Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic) };
-            string jsonString = JsonSerializer.Serialize(langsMessage, options);
-            File.WriteAllText(fileName, jsonString);
-
-            jsonBox.Text = File.ReadAllText(fileName);
-            
-            //greetingsMessage.Content = "";
+            InitializeLanguageSelector();
         }
 
         private void cmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var timeMessage = new TimeGreetings();
+            GetMessage();
+        }
 
-            greetingsMessage.Content = cmb.SelectedIndex switch
+        internal void GetMessage()
+        {
+            var langs = ReadLangs().LangMessages;
+            var hourNow = DateTime.Now.Hour;
+
+            greetingsMessage.Content = hourNow switch
             {
-                0 => timeMessage.GetMessage(),
-                1 => "Hallo Welt",
-                2 => "Привет мир",
-                3 => "Сәлам Дөнья",
-                _ => "Зарецкий, уйди нахуй отсюда"
+                >= 5 and < 10 => langs[cmb.SelectedValue.ToString()].Evening,
+                >= 10 and < 18 => langs[cmb.SelectedValue.ToString()].Day,
+                >= 18 and < 23 => langs[cmb.SelectedValue.ToString()].Evening,
+                >= 23 or < 5 => langs[cmb.SelectedValue.ToString()].Night,
             };
+        }
+
+        private void InitializeLanguageSelector()
+        {
+            LangsCollection langs = ReadLangs();
+
+            foreach (var item in langs.LangMessages)
+            {
+                cmb.Items.Add(item.Key);
+            }
+
+            cmb.SelectedIndex = 0;
+        }
+
+        private LangsCollection ReadLangs()
+        {
+            string fileName = "langs.json";
+            string jsonString = File.ReadAllText(fileName);
+            return JsonSerializer.Deserialize<LangsCollection>(jsonString);
         }
     }
 }
